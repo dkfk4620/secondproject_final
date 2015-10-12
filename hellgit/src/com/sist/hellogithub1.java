a414e154ea2c1bc0a59ee8f1e6484477df080930
ara jo <dkfk4620@naver.com>
2015-10-12 16:50:49 +0900

    github1 - 이클립스에서 수정

--- hellgit/src/com/sist/hellogithub1.java
+++ hellgit/src/com/sist/hellogithub1.java
@@ -5,6 +5,7 @@ public class hellogithub1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("github1");
+		System.out.println("이클립스에서 수정");
 	}
 
 }
