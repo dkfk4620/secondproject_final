ab69b4977edface1288be7c9b5b6c3fc08b5db76
dkfk4620 <dkfk4620@naver.com>
2015-10-12 16:47:50 +0900

    Update hellogithub1.java

--- hellgit/src/com/sist/hellogithub1.java
+++ hellgit/src/com/sist/hellogithub1.java
@@ -5,6 +5,7 @@ public class hellogithub1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("github1");
+		System.out.println("gitHub에서 수정함");
 	}
 
 }
